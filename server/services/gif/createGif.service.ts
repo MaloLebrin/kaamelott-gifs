41adb9f82f802148ec34ef9c10794c4e95b35341
MaloLebrin <malolebrin@gmail.com>
2025-04-15 12:05:50 +0200

    ✨ Implement createManyGif service and Cloudinary upload utility for GIF management. This update introduces a new service for creating multiple GIFs by uploading them to Cloudinary, enhancing the application's media handling capabilities. The addition of the uploadGifToCloudinary function in the cloudinary utility file further improves the scalability and maintainability of the codebase, allowing for efficient GIF uploads and better organization of media assets.

---
+++ server/services/gif/createGif.service.ts
@@ -0,0 +1,31 @@
+import { uploadGifToCloudinary } from "~/server/utils/cloudinary"
+import { Entities, Gif } from "~/types"
+
+/**
+ * Create many gifs
+ * @param gifs - The gifs to create
+ * @param client - The supabase client
+ * @returns The created gifs
+ */
+export async function createManyGif({
+  gifs,
+  client,
+}: {
+  gifs: (Gif & { filePath: string })[],
+  client: any
+}) {
+  const uploadedGifs = await Promise.all(gifs.map(async (gif) => {
+    return uploadGifToCloudinary(gif, gif.filePath)
+  }))
+
+  const { data, error } = await client
+    .from(Entities.GIF)
+    .insert(uploadedGifs)
+    .select()
+
+  if (error) {
+    throw createError({ statusMessage: error.message })
+  }
+
+  return data
+}
---
+++ server/utils/cloudinary.ts
@@ -0,0 +1,42 @@
+import { Gif } from "~/types"
+import cloudinary from 'cloudinary'
+
+/**
+ * Cloudinary client
+ * @returns The cloudinary client
+ */
+export const imageClient = cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+/**
+ * Upload a gif to Cloudinary
+ * @param gif - The gif to upload
+ * @param filePath - The path to the gif file
+ * @returns The uploaded gif
+ */
+export const uploadGifToCloudinary = async (gif: Gif, filePath?: string) => {
+  if (!imageClient) {
+    throw new Error('Cloudinary is not configured')
+  }
+  // const filePath = path.join(process.cwd(), 'public', 'gifs', `${gif.filename}`)
+
+  const result = await imageClient.uploader.upload(filePath, {
+    resource_type: 'auto',
+    folder: 'kaamelott-gifs',
+    public_id: gif.slug,
+    overwrite: false,
+    transformation: [
+      { quality: 'auto' },
+      { fetch_format: 'auto' }
+    ]
+  })
+
+  return {
+    ...gif,
+    url: result.secure_url,
+    public_id: result.public_id
+  }
+}
