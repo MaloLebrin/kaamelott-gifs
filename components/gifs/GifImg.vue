1109925ab08043dbdca8c515318e3ba2ebc9ef28
MaloLebrin <malolebrin@gmail.com>
2025-04-15 08:10:02 +0200

    ✨ Add GifImg component for optimized image loading with NuxtImg. This new component enhances the user experience by displaying a placeholder while the actual image loads, contributing to a more responsive and visually appealing interface. The implementation follows best practices for maintainability and scalability within the application.

---
+++ components/gifs/GifImg.vue
@@ -0,0 +1,30 @@
+<template>
+  <NuxtImg
+  src="/images/nuxt.png"
+  alt="image"
+  width="400"
+  height="400"
+  :custom="true"
+  v-slot="{ src, isLoaded, imgAttrs }"
+>
+  <!-- Show the actual image when loaded -->
+  <img
+    v-if="isLoaded"
+    v-bind="imgAttrs"
+    :src="src"
+    alt="gif"
+  >
+
+  <!-- Show a placeholder while loading -->
+  <img
+    v-else
+    src="https://placehold.co/400x400"
+    alt="placeholder"
+  >
+</NuxtImg>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+  src: string
+}>()
+</script>
