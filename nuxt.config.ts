333e5ae929a8e3e5b5b0ff21302bc908f1ef4360
MaloLebrin <malolebrin@gmail.com>
2025-04-16 14:28:06 +0200

    refactor: update route rules in nuxt.config.ts to enable prerendering for the '/about' page, enhancing the application's performance and maintainability by optimizing rendering strategies.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   routeRules: {
-    '/': { prerender: true },
+    '/about': { prerender: true },
     '/characters/**': { swr: true },
   },
   modules: [
