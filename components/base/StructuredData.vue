1c465c3b5ce832401e9f34771ecb0597f28924db
MaloLebrin <malolebrin@gmail.com>
2025-04-18 18:28:56 +0200

    ✨ Add StructuredData component and useStructuredData composable for enhanced SEO optimization. The new StructuredData.vue component integrates structured data in JSON-LD format, improving search engine visibility. Additionally, the useStructuredData.ts file introduces a function to generate structured data for GIFs, contributing to better organization and maintainability of SEO-related data within the codebase. These changes ensure a scalable approach to handling structured data, aligning with the project's focus on SEO and user engagement.

---
+++ components/base/StructuredData.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { GifStructuredData, CharacterStructuredData, EpisodeStructuredData } from '~/types/seo'
+
+interface Props {
+  data: GifStructuredData | CharacterStructuredData | EpisodeStructuredData
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <script type="application/ld+json">
+    {{ JSON.stringify(props.data) }}
+  </script>
+</template> 
