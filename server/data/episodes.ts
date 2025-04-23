b02878847c6921d1ed2529a72ee0a7213284d49f
MaloLebrin <malolebrin@gmail.com>
2025-04-23 17:16:45 +0200

    ✨ Add seasonFour episode data by creating seasonFour.ts and updating episodes.ts to include its content, enhancing the modularity and scalability of episode information. This update aligns with the project's focus on comprehensive content management and improving user engagement through richer episode data.

--- server/data/episodes.ts
+++ server/data/episodes.ts
@@ -2,9 +2,12 @@
 import { seasonOne } from "~/server/data/episodes/seasonOne"
 import { seasonTwo } from "~/server/data/episodes/seasonTwo";
 import { seasonThree } from "~/server/data/episodes/seasonThree";
+import { seasonFour } from "~/server/data/episodes/seasonFour";
 import { SeedEpisode } from "~/server/data/episodes/index";
+
 export const episodes: SeedEpisode[] = [
   ...seasonOne, // 100 épisodes
   ...seasonTwo, // 100 épisodes
   ...seasonThree, // 100 épisodes
+  ...seasonFour, // 100 épisodes
 ];
---
+++ server/data/episodes/seasonFour.ts
@@ -0,0 +1,5 @@
+import { SeedEpisode } from "~/server/data/episodes/index";
+
+export const seasonFour: SeedEpisode[] = [
+
+]
