112bcf2f4b398f0130cf2504c30af94f04f3daf0
MaloLebrin <malolebrin@gmail.com>
2025-04-23 17:37:19 +0200

    ✨ Add detailed episode data for Season Five in seasonFive.ts, including titles, resumes, and character lists for each episode, further enhancing content richness and user engagement. This update continues to align with the project's focus on providing comprehensive information and improving the overall user experience by offering more context for each episode while maintaining the modularity and scalability of the episode data structure.

---
+++ server/data/episodes/seasonFive.ts
@@ -0,0 +1,52 @@
+import { SeedEpisode } from "~/server/data/episodes/index";
+
+export const seasonFive: SeedEpisode[] = [
+  {
+    "code": "S05E01",
+    "title": "Corvus Corone",
+    "resume": "Six mois après la victoire d'Arthur sur les hommes de Lancelot et le sauvetage de Guenièvre, la vie quotidienne à Kaamelott semble avoir repris son cours normal. Les traîtres orcaniens arrivent à Kaamelott pour demander pardon. Arthur n'arrive pas à se décider à les condamner à mort. Pendant ce temps, Perceval et Karadoc entreprennent de déménager leurs meubles du château à la taverne pour rembourser leurs dettes. Lancelot, tapi dans une grotte perdue dans les montagnes, est toujours vivant et le mystérieux homme en noir continue de le pervertir.",
+    "characters": "Arthur,Lancelot,Guenièvre,Loth,Gauvain,Galessin,Dagonet,Perceval,Karadoc,Le Tavernier,Léodagan,Calogrenant,Bohort,Méléagant"
+  },
+  {
+    "code": "S05E02",
+    "title": "La Roche et le Fer",
+    "resume": "Arthur se rend au rocher pour y replanter Excalibur. En chemin, il tombe sur Venec qui tente de l'en dissuader. La Dame du Lac, qui vit désormais en vagabonde, est désespérée par le manque de foi d'Arthur. Excalibur est plantée dans son rocher et tous les grands du royaume se demandent s'ils doivent tenter de la retirer. La quête du Graal est plus que jamais au point mort : entre les défections et la formation des clans autonomes, il ne reste plus de chevaliers à Kaamelott à part Léodagan et Bohort.",
+    "characters": "Arthur,Venec,La Dame du Lac,Léodagan,Bohort,Séli,Ygerne,Cryda,Elias,Merlin"
+  },
+  {
+    "code": "S05E03",
+    "title": "Vae Soli!",
+    "resume": "Tous les souverains du monde connu affluent à Kaamelott pour retirer Excalibur du rocher. Le duc d'Aquitaine et son épouse arrivent, créant des tensions à la cour. Le clan de Perceval et Karadoc s'agrandit et ils sont obligés de dormir dans la même chambre d'auberge. Karadoc veut tenter de récupérer Excalibur, et Perceval, blessé dans la fidélité qu'il porte au roi, s'enferme dans un tonneau.",
+    "characters": "Arthur,Léodagan,Séli,Le Duc d'Aquitaine,La Duchesse d'Aquitaine,Perceval,Karadoc,Mevanwi,Mehgan,Merlin,Yvain,Gauvain"
+  },
+  {
+    "code": "S05E04",
+    "title": "Le Dernier Jour",
+    "resume": "Le duc d'Aquitaine est copieusement insulté par sa femme. Arthur et Guenièvre se disputent. Guenièvre souhaite crever l'abcès entre Mevanwi et elle. Loth, Galessin et Dagonet font route vers la roche où est plantée Excalibur. Le roi Loth échoue à la tentative de retirer Excalibur du rocher. Arthur se rend compte que ni le duc d'Aquitaine ni Bohort n'ont tenté de retirer l'épée du rocher.",
+    "characters": "Arthur,Guenièvre,Mevanwi,Loth,Galessin,Dagonet,Le Duc d'Aquitaine,La Duchesse d'Aquitaine,Bohort,Léodagan,Séli,Yvain,Gauvain"
+  },
+  {
+    "code": "S05E05",
+    "title": "Le Royaume Sans Tête",
+    "resume": "Le Royaume de Logres n'a plus de roi. Les grands essaient tant bien que mal de s'organiser, et Arthur refuse catégoriquement de les aider. Le Jurisconsulte arrive et est catastrophé de l'état des archives. Léodagan influence Guenièvre pour qu'elle le désigne en tant que régent du royaume de Logres. Léodagan est officiellement roi-régent de Bretagne et rend les réunions de la Table Ronde obligatoires.",
+    "characters": "Arthur,Guenièvre,Léodagan,Le Jurisconsulte,Séli,Bohort,Lionel de Gaunes,Le Père Blaise,Elias,Le Maître d'Armes"
+  },
+  {
+    "code": "S05E06",
+    "title": "Jizô",
+    "resume": "Arthur et Guenièvre font halte à la taverne, le quartier général des Semi-Croustillants. La régence de Léodagan fait craindre à Loth d'Orcanie et à sa cruelle femme Anna la levée du sursis. Arthur et Guenièvre font route avec le tavernier. Arthur retrouve dans un phare de la côte le père des Jumelles du pêcheur et lui demande si une de ses filles a été enceinte.",
+    "characters": "Arthur,Guenièvre,Perceval,Karadoc,Merlin,Loth,Anna,Le Tavernier,Le Père des Jumelles,Guethenoc,Roparzh"
+  },
+  {
+    "code": "S05E07",
+    "title": "Le Phare",
+    "resume": "Arthur passe la nuit au phare en attendant le guide qui le ramènera à Kaamelott. Il fait un cauchemar intense. Le guide recueille Arthur. Il sait qui il est, car ce guide n'est autre que Méléagant. Arthur et son guide Méléagant arrivent chez un vieillard nommé Anton, qui avait caché Arthur pendant son enfance.",
+    "characters": "Arthur,Méléagant,Anton,Le Fils d'Anton,Le Père des Jumelles"
+  },
+  {
+    "code": "S05E08",
+    "title": "Le Garçon qui Criait au Loup",
+    "resume": "Mevanwi exige que Karadoc, en tant que roi de Bretagne, prenne un bain régulièrement. Karadoc décide d'imiter Arthur en prenant des maîtresses. Arthur est fâché avec son guide Méléagant. Lancelot a aussi pris un bain et est guéri. Arthur retrouve Prisca la voyante dans une troupe de saltimbanques. Elle lui révèle qu'il est infécond. Arthur retourne à Kaamelott, totalement déprimé. Lancelot le retrouve dans la salle de bain où il s'est tranché les veines du poignet.",
+    "characters": "Arthur,Mevanwi,Karadoc,Lancelot,Méléagant,Prisca la voyante,Aelis,Guenièvre,Bohort"
+  }
+]
