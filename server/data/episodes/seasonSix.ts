4205fa4a8e6e0f446fccbe379f175e97f19ad41e
MaloLebrin <malolebrin@gmail.com>
2025-04-23 18:18:09 +0200

    ✨ Add detailed episode data for Season Six in seasonSix.ts, including titles, resumes, and character lists for each episode, further enhancing content richness and user engagement. This update continues to align with the project's focus on providing comprehensive information and improving the overall user experience by offering more context for each episode while maintaining the modularity and scalability of the episode data structure.

---
+++ server/data/episodes/seasonSix.ts
@@ -0,0 +1,58 @@
+import { SeedEpisode } from "~/server/data/episodes/index";
+
+export const seasonSix: SeedEpisode[] = [
+  {
+    "code": "S06E01",
+    "title": "Miles Ignotus",
+    "resume": "Rome rencontre de grandes difficultés en Bretagne. Le chef de guerre du camp fortifié du mur d'Hadrien, Manius Macrinus Firmus, en poste depuis treize ans, a subi plusieurs défaites contre les indigènes de ce pays. Le sénateur Lucius Silius Sallustius décide de régler le « problème breton » en se servant de la légende de « l'épée des rois ». À la recherche d'un Breton parmi les soldats romains, il recrute Arturus, soldat de la milice urbaine, qui n'a plus aucun souvenir de la Bretagne. Arturus et Manilius, un autre soldat de la milice et ami d'Arturus, font le mur et s'introduisent illégalement dans une fête bourgeoise ayant lieu dans la villa Aconia.",
+    "characters": "Arturus,Manilius,Manius Macrinus Firmus,Lucius Silius Sallustius,Glaucia,Aconia Minor"
+  },
+  {
+    "code": "S06E02",
+    "title": "Centurio",
+    "resume": "La Dame du Lac est convoquée par les Dieux pour lui rappeler sa mission de préparation d'Arturus au trône et à la quête du Graal. Après l'incident à la villa Aconia, où Arturus frappe Glaucia, Sallustius décide de le prendre sous sa protection et le nomme centurion afin de le sauver de la punition qui l'attend. Manilius, présent lors des événements, prend la fuite.",
+    "characters": "Arturus,Manilius,La Dame du Lac,Lucius Silius Sallustius,Glaucia,Aconia Minor,Les Dieux"
+  },
+  {
+    "code": "S06E03",
+    "title": "Præceptores",
+    "resume": "Sallustius confie Arturus à Aconia Minor afin qu'elle l'éduque et l'instruise, car il devient de plus en plus évident qu'Arturus est la solution du « problème breton ». De leur côté, Merlin est en visite à Rome, Léodagan est invité à s'y rendre par Glaucia, et Arturus demande à Merlin de rentrer en Bretagne pour signifier à quiconque qu'il peut se faire connaître par un fait d'armes ou une quête de son choix afin de pouvoir gouverner à ses côtés.",
+    "characters": "Arturus,Aconia Minor,Merlin,Léodagan,Glaucia,Lucius Silius Sallustius,Les Dieux"
+  },
+  {
+    "code": "S06E04",
+    "title": "Arturi inquisito",
+    "resume": "Arturus, toujours sous la protection de Sallustius, se voit confier une mission : tuer un chef ostrogoth afin de devenir dux bellorum (chef de guerre). Après une longue hésitation, il accepte à la seule condition qu'Appius Manilius soit libéré de prison, tandis que Merlin doit demander à tout le peuple breton d'accomplir une quête afin de participer au gouvernement de la Bretagne avec Arturus.",
+    "characters": "Arturus,Appius Manilius,Merlin,Lucius Silius Sallustius,Le Chef Ostrogoth,Le Peuple Breton"
+  },
+  {
+    "code": "S06E05",
+    "title": "Dux bellorum",
+    "resume": "Arturus est nommé chef de guerre mais ce poste lui déplaît, car il sait que c'est moins dû à son mérite qu'à l'objectif de le faire parvenir au poste de roi de Bretagne. Il discute avec César, qui lui donne plusieurs « conseils ». Arturus cultive une passion pour Aconia, sa préceptrice, tandis que de leur côté, Perceval, Karadoc, Bohort et Lancelot essaient d'accomplir une quête.",
+    "characters": "Arturus,César,Aconia Minor,Perceval,Karadoc,Bohort,Lancelot,Lucius Silius Sallustius"
+  },
+  {
+    "code": "S06E06",
+    "title": "Nuptiæ",
+    "resume": "Arturus apprend que Léodagan de Carmélide ne voudra rallier son royaume à celui de Logres que si le futur roi épouse sa fille, Guenièvre, tandis qu'Aconia revient sur sa décision et épouse Arturus, bien qu'elle soit déjà mariée. Dans les rues du ghetto de Rome, Arturus subit tant bien que mal les caprices puérils de César. La situation atteint son paroxysme lorsque ce dernier déclare avoir déféqué dans sa tunique. Finalement Arturus rend sa dignité à César en le faisant parader dans les rues de Rome en cuirasse de général. Revenu à la villa Aconia, Arturus fait la promesse à sa nouvelle épouse qu'il ne consommera jamais le mariage potentiel avec Guenièvre.",
+    "characters": "Arturus,Léodagan,Guenièvre,Aconia Minor,César,Lucius Silius Sallustius,Le Mari d'Aconia"
+  },
+  {
+    "code": "S06E07",
+    "title": "Arturus rex",
+    "resume": "Arturus et Appius Manilius arrivent en Bretagne et rencontrent Perceval, Karadoc et les autres candidats pour devenir chevaliers, tandis que l'ancien chef de guerre romain qui gouvernait la Bretagne rentre chez lui, en souriant. Arturus retire Excalibur du rocher puis se rend au rassemblement des chefs de clans, où Léodagan le reçoit. Puis le roi Arthur découvre sa promise, Guenièvre.",
+    "characters": "Arturus,Appius Manilius,Perceval,Karadoc,Léodagan,Guenièvre,Manius Macrinus Firmus,Les Chefs de Clans,Les Candidats Chevaliers"
+  },
+  {
+    "code": "S06E08",
+    "title": "Lacrimosa",
+    "resume": "Arthur et Guenièvre se marient, puis ce dernier réunit tout le peuple breton sur une plage. Astucieusement, il démontre à Sallustius que le peuple breton est prêt à tout pour se défaire du joug romain et explique donc au sénateur qu'il prend le plein contrôle de la Bretagne. Sallustius et Capito lui font comprendre qu'il sera traqué et tué s'il revient à Rome. Arthur explique au peuple breton qu'il est libéré du joug romain, avec des difficultés de communication avec Pellinor. Manilius est assassiné par Procyon, informé par Verinus. César, sous la pression de Méléagant, se suicide.",
+    "characters": "Arthur,Guenièvre,Lucius Silius Sallustius,Capito,Manilius,Procyon,Verinus,César,Méléagant,Pellinor,Le Peuple Breton"
+  },
+  {
+    "code": "S06E09",
+    "title": "Dies iræ",
+    "resume": "Six à douze mois après sa tentative de suicide, Arthur voit ses chevaliers, sa femme et sa demi-sœur défiler à son chevet, à la forteresse de Tintagel. Tous découvrent qu'Arthur est encore vivant, mais trop faible pour régner de nouveau. Karadoc lui rend le pouvoir qu'Arthur confie finalement à Lancelot. Celui-ci retombe sous l'emprise de Méléagant et sème le chaos en Bretagne. Pendant que Lancelot et ses hommes sortent la Table Ronde de Kaamelott et la brûlent, Venec tente de transporter Arthur dans un lieu sûr. Arthur accepte de partir à Rome où il sera le plus en sécurité. Dans la maison d'Aconia laissée à l'abandon, Arthur reprend confiance en lui et se prépare à repartir au combat.",
+    "characters": "Arthur,Guenièvre,Karadoc,Lancelot,Méléagant,Venec,Aconia Minor,Les Chevaliers,La Demi-Sœur d'Arthur,Les Hommes de Lancelot"
+  }
+]
