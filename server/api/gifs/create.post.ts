97318b7007f82f03d777e8fefafe369630507f90
MaloLebrin <malolebrin@gmail.com>
2025-05-04 01:08:59 +0200

    🔧 Update AWS S3 integration in GIF upload process by renaming environment variables for clarity and consistency. This change enhances the maintainability of the codebase by standardizing the naming convention for AWS credentials and bucket name, ensuring better readability and reducing potential confusion in the configuration. Additionally, the region is now explicitly set to 'eu-west-3', improving clarity in the AWS client setup.

--- server/api/gifs/create.post.ts
+++ server/api/gifs/create.post.ts
@@ -9,12 +9,12 @@ import { Gif } from '~/types/Gif'
 
 export default defineEventHandler(async (event) => {
   try {
-    const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY
-    const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY
-    const AWS_REGION = process.env.AWS_REGION
-    const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME
+    const AWS_S3_ACCESS_KEY = process.env.AWS_S3_ACCESS_KEY
+    const AWS_S3_SECRET_KEY = process.env.AWS_S3_SECRET_KEY
+    const AWS_S3_REGION = 'eu-west-3'
+    const AWS_S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME
   
-    if (!AWS_ACCESS_KEY || !AWS_SECRET_KEY || !AWS_REGION || !AWS_BUCKET_NAME) {
+    if (!AWS_S3_ACCESS_KEY || !AWS_S3_SECRET_KEY || !AWS_S3_REGION || !AWS_S3_BUCKET_NAME) {
       console.error('AWS credentials are not set')
       throw createError({
         statusCode: 500,
--- server/services/aws/client.ts
+++ server/services/aws/client.ts
@@ -4,10 +4,10 @@ import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
  * AWS S3 client
  */
 export const awsClient = new S3Client({
-  region: process.env.AWS_REGION || 'eu-west-3',
+  region: 'eu-west-3',
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY || '',
-    secretAccessKey: process.env.AWS_SECRET_KEY || '',
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY || '',
+    secretAccessKey: process.env.AWS_S3_SECRET_KEY || '',
   },
 })
 
@@ -18,8 +18,8 @@ export const awsClient = new S3Client({
  */
 export async function uploadGifToS3(file: any, fileName: string) {
   try {
-    if (!process.env.AWS_BUCKET_NAME) {
-      throw new Error('AWS_BUCKET_NAME is not set')
+    if (!process.env.AWS_S3_BUCKET_NAME) {
+      throw new Error('AWS_S3_BUCKET_NAME is not set')
     }
   
     if (!file) {
@@ -27,7 +27,7 @@ export async function uploadGifToS3(file: any, fileName: string) {
     }
   
     const params = {
-      Bucket: process.env.AWS_BUCKET_NAME || '',
+      Bucket: process.env.AWS_S3_BUCKET_NAME || '',
       Key: `gifs/${fileName}`,
       ContentType: file.type,
       Body: file.data, 
