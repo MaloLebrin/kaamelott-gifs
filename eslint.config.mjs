de822ebdd87595d3e618cc33abd2f88ff1eeaf50
MaloLebrin <malolebrin@gmail.com>
2025-05-04 12:44:13 +0200

    🧑‍💻 Update ESLint configuration to enforce Vue component structure rules, including component tags order and maximum attributes per line. This enhancement improves code consistency and maintainability across the project by ensuring adherence to best practices in Vue component development.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,17 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  rules: {
+    'vue/component-tags-order': ['error', {
+      order: ['template', 'script', 'style']
+    }],
+    'vue/max-attributes-per-line': ['error', {
+      singleline: 1,
+      multiline: {
+        max: 1,
+        allowFirstLine: false
+      }
+    }]
+  }
+})
